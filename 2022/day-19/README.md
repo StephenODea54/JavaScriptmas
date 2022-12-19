db2e1f181fecc0b57be316f483948875013f7215
Stephen O'Dea <odeastephen1@gmail.com>
2022-12-19 17:07:23 +0000

    Added source code, instructions, and solution to day 19.

---
+++ 2022/day-19/README.md
@@ -0,0 +1,19 @@
+# 📆 Century From Year
+
+Given a year, return the century it is in. The first century spans from the year $1$ up to and including the year $100$, the second - from the year $101$ and up to and including the year $2000$, etc.
+
+## Example 1
+
+```js
+const input = 1905;
+const result = centuryFromYear(input);
+console.log(result) // 20
+```
+
+## Example 2
+
+```js
+const input = 1700;
+const result = centuryFromYear(input);
+console.log(result) // 17
+```
---
+++ 2022/day-19/main.js
@@ -0,0 +1,39 @@
+const centuryFromYear = (num) => {
+    const numDiv = Math.floor(num / 100);
+    const numMod = num % 100;
+    
+    return numMod === 0 ? numDiv : numDiv + 1;
+}
+
+/**
+* Test Suite 
+*/
+describe('centuryFromYear()', () => {
+    it('returns current century', () => {
+        // arrange
+        const year = 1905;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(20);
+    });
+    
+    it('returns current century for edge case of start of century', () => {
+        // arrange
+        const year = 1700;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(17);
+    });    
+});
